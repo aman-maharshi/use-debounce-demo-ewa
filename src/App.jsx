63ff0a47d152aa9e1ae56ea8109b8053cbe1925d
aman-maharshi <amanm4harshi@gmail.com>
2025-01-04 16:52:21 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Home from './Home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/other" element={<OtherComponent />} /> */}
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/Home.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+import { Cross, Search } from "./icons"
+import { useLocation } from 'react-router-dom';
+
+const Home = () => {
+
+  const location = useLocation()
+  const params = new URLSearchParams(location.search)
+  const query = params.get('search') || ''
+  const [search, setSearch] = useState(query)
+
+  const handleSearch = (e) => {
+    const newSearch = e.target.value
+    setSearch(newSearch)
+    const params = new URLSearchParams(location.search)
+    if (newSearch) {
+      params.set('search', newSearch)
+    } else {
+      params.delete('search')
+    }
+    window.history.replaceState({}, '', `${location.pathname}?${params.toString()}`)
+  }
+
+
+
+  return (
+    <div className='h-screen flex justify-center items-center bg-[#393939] text-white'>
+
+      <div className='bg-[#121212] p-6 rounded-lg shadow-md shadow-gray-800 flex items-center gap-4 w-[500px]'>
+        <Search />
+        <input
+          value={search}
+          onChange={handleSearch}
+          type="text"
+          className='flex-1 bg-transparent outline-none border-none text-lg'
+        />
+
+        <button className='cursor-pointer'>
+          <Cross />
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Home
